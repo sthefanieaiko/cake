937cf2cba5f9aa7ef21cddf229c932af6284cc05
sthefanieaiko <112731883+sthefanieaiko@users.noreply.github.com>
2023-10-17 22:01:43 -0300

    done

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import Comanda from "./screens/Comanda";
 import Login from "./screens/Login";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Produtos from "./screens/Produtos";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ export default function App() {
       <Stack.Navigator screenOptions={{headerShown:false}}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Comanda" component={Comanda} />
+        <Stack.Screen name="Produtos" component={Produtos} />
       </Stack.Navigator>
     </NavigationContainer>
   );
