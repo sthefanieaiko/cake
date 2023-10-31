47ec233cc3d0c71ac5b3b82c6077d33813e591fe
sthefanieaiko <112731883+sthefanieaiko@users.noreply.github.com>
2023-10-31 19:08:09 -0300

    Update Login.js

--- screens/Login.js
+++ screens/Login.js
@@ -2,20 +2,37 @@ import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Button from '../components/button';
 import Input from '../components/input';
+import { useContext, useState } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
 export default function Login({navigation}) {
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+  const [erro, setErro] = useState('')
+  const {login} = useContext(AuthContext)
+
+  async function handleLogin(){
+    if (await login({email, senha})){
+      navigation.navigate("Comanda")
+    }else{
+      setErro("usuário ou senha inválidos")
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Image style={styles.image} source={require('../assets/home.jpg')} />
       <Text>Sistema</Text>
       <Text style={styles.title}>Comanda</Text>
 
-      <Input placeholder='e-mail'/>
-      <Input placeholder='senha' secureTextEntry/>
+      <Input placeholder='e-mail' value={email} onChangeText={setEmail}/>
+      <Input placeholder='senha'value={senha} onChangeText={setSenha} secureTextEntry/>
 
-      <Button onPress={() => navigation.navigate('Comanda')}>
+      <Button onPress={handleLogin}>
         Entrar
       </Button>
+
+      <Text>{erro}</Text>
       
       <TouchableOpacity >
         <Text>criar conta</Text>
